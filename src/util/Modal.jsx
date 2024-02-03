47a0d86f4d8982e4484194e4265b2b173de98d83
Oksana Kormilina <keysb@mail.ru>
2024-02-03 20:20:01 +0400

    Add opening and closing modal

--- src/util/Modal.jsx
+++ src/util/Modal.jsx
@@ -1,11 +1,21 @@
+import { useRef, useEffect } from "react";
+
 import Button from "./Button";
 
 export default function Modal({ children, declineBtn, submitBtn }) {
+const dialog = useRef()
+
+useEffect(() => {if(dialog.current) dialog.current.showModal()})
+// 
+
+function handleClose() {
+  dialog.current.close()
+}
   return (
-    <dialog className="modal" open>
+    <dialog ref={dialog} className="modal">
       {children}
       <div className="modal-actions">
-  {declineBtn && <button className="text-button">{declineBtn}</button>}
+  {declineBtn && <button onClick={handleClose} className="text-button">{declineBtn}</button>}
     <Button onClick={() => handleAddToCart(MEAL)}>{submitBtn}</Button>
   </div>
     </dialog>
