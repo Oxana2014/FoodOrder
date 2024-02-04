6a9f0f25f980548a48de1878cf0c6ce7a8491007
Oksana Kormilina <keysb@mail.ru>
2024-02-04 18:52:44 +0400

    Add sending order to backend

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -8,25 +8,52 @@ export default function OrderForm({
   onErrorAfterSubmit,
   submitOrder,
 }) {
+  const { items: mealsInOrder } = useContext(CartContext);
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    street: "",
+    ["postal-code"]: "",
+    city: "",
+  });
 
-const {items: mealsInOrder} = useContext(CartContext)
-const [data, setData] = useState({name: '', email: "", street: "", ['postal-code']: "", city: ""})
+  const dataIsReady =
+    data.name && data.email && data.street && data["postal-code"] && data.city;
+  const totalPrice = mealsInOrder.reduce(
+    (acc, meal) => acc + meal.price * meal.quantity,
+    0
+  );
 
-const dataIsReady =  data.name && data.email && data.street && data['postal-code'] && data.city
-const totalPrice = mealsInOrder.reduce((acc, meal) => acc + meal.price * meal.quantity, 0)
+  function handleChange(input, value) {
+    setData((prevData) => ({ ...prevData, [input]: value }));
+  }
 
-function handleChange(input, value) {
-  setData((prevData) =>({...prevData, [input]: value }) )
-}
+  function onSubmit() {
+    const order = {};
+    order.items = mealsInOrder;
+    order.customer = data;
 
-function onSubmit() {
+    async function sendOrder(order) {
+      console.log("order: ", order)
+      try {
+        const response = await fetch("http://localhost:3000/orders", {
+          method: "POST",
+          body: JSON.stringify({order}),
+          headers: {  'Content-Type': 'application/json' },
+        });
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error("Can not send the order");
+        }
+        console.log("Receive response: ", responseData);
+        submitOrder();
+      } catch (error) {
+        onErrorAfterSubmit(`An error occured: ${error}`);
+      }
+    }
+    sendOrder(order)
     console.log("Send http request");
     console.log("Receive response");
-    //if (!responce.ok)
-    // throw error
-   // onErrorAfterSubmit("An error occured");
-    //else
-    submitOrder();
   }
 
   return (
@@ -37,29 +64,66 @@ function onSubmit() {
       submitFn={onSubmit}
       submitDisabled={!dataIsReady}
     >
-      <form >
+      <form>
         <h3>Checkout</h3>
         <p>TotalAmount ${totalPrice}</p>
         <div className="control">
           <label htmlFor="name">Full Name</label>
-          <input onChange={(event) => handleChange('name', event.target.value)} value={data.name} type="text" id="name" name="name" required/>
+          <input
+            onChange={(event) => handleChange("name", event.target.value)}
+            value={data.name}
+            type="text"
+            id="name"
+            name="name"
+            required
+          />
         </div>
         <div className="control">
           <label htmlFor="email">E_Mail Address</label>
-          <input  onChange={(event) => handleChange('email', event.target.value)} value={data.email} type="email" id="email" name="email" required/>
+          <input
+            onChange={(event) => handleChange("email", event.target.value)}
+            value={data.email}
+            type="email"
+            id="email"
+            name="email"
+            required
+          />
         </div>
         <div className="control">
           <label htmlFor="street">Street</label>
-          <input  onChange={(event) => handleChange('street', event.target.value)}  value={data.street} type="text" id="street" name="street" required/>
+          <input
+            onChange={(event) => handleChange("street", event.target.value)}
+            value={data.street}
+            type="text"
+            id="street"
+            name="street"
+            required
+          />
         </div>
         <div className="control-row">
           <div className="control">
             <label htmlFor="postal-code">Postal Code</label>
-            <input  onChange={(event) => handleChange('postal-code', event.target.value)}  value={data["postal-code"]} type="text" id="postal-code" name="postal-code" required/>
+            <input
+              onChange={(event) =>
+                handleChange("postal-code", event.target.value)
+              }
+              value={data["postal-code"]}
+              type="text"
+              id="postal-code"
+              name="postal-code"
+              required
+            />
           </div>
           <div className="control">
             <label htmlFor="city">City</label>
-            <input  onChange={(event) => handleChange('city', event.target.value)} value={data.city} type="text" id="city" name="city" required/>
+            <input
+              onChange={(event) => handleChange("city", event.target.value)}
+              value={data.city}
+              type="text"
+              id="city"
+              name="city"
+              required
+            />
           </div>
         </div>
       </form>
