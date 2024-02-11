a778dcf72549ff4258532f9504fb7d1313cb8e58
Oksana Kormilina <keysb@mail.ru>
2024-02-11 21:46:14 +0400

    Add quantity on cart button

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
 import logo from "../assets/logo.jpg";
 import Button from "../util/Button";
+import { CartContext } from "../store/cart-context";
 
 export default function MainHeader({openCart}) {
+const {items: cartItems} = useContext(CartContext)
+const totalQuantity = cartItems.reduce((acc, item) => acc+ item.quantity, 0)
 
      return (
     <div id="main-header">
@@ -10,7 +14,7 @@ export default function MainHeader({openCart}) {
  </div>
         <h1>Food Order App</h1>
         
-        <Button onClick={openCart}>Cart</Button>
+        <Button onClick={openCart}>Cart ({totalQuantity})</Button>
     </div>
   );
 }
